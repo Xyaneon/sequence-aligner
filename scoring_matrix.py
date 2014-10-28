fe5e9770e8571e908218833142420bd6b4affe1e
Christopher Kyle Horton <christhehorton@gmail.com>
2014-10-28 17:05:00 -0400

    Compactify code in scoring_matrix.__init()__.

--- scoring_matrix.py
+++ scoring_matrix.py
@@ -24,4 +24,5 @@ class scoring_matrix:
     def __init__(self, sequence_length1, sequence_length2):
         '''Initializes a new scoring matrix with the supplied sequence
         lengths.'''
-        self.matrix = [[0 for x in range(sequence_length1)] for x in sequence_length2]
+        seql = (sequence_length1 + 1, sequnece_length2 + 1)
+        self.matrix = [[0 for x in range(seql[0])] for x in range(seql[1])]
