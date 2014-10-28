3ea107c37efd283d811b2d2f3e0720f93bad35ff
Christopher Kyle Horton <christhehorton@gmail.com>
2014-10-28 17:24:33 -0400

    Add a ScoringMatrixCell class.

--- scoring_matrix.py
+++ scoring_matrix.py
@@ -19,10 +19,61 @@
 # Christopher Kyle Horton (000516274), chorton@ltu.edu
 # Last modified: 10/28/2014
 
+class ScoringMatrixCell:
+    '''A class implementing individual cells within the scoring matrix.'''
+    def __init__(self, score=0, up=False, diagonal=False, left=False):
+        '''Initializes this ScoringMatrixCell instance.
+
+        If the arguments are omitted, a cell with no backlinks and a score of
+        0 will be created by default.'''
+        self.score = score
+        self.up = up
+        self.diagonal = diagonal
+        self.left = left
+
+    def get_score(self):
+        '''Returns this cell's current score.'''
+        return self.score
+
+    def get_backlinks(self):
+        '''Returns a dictionary containing this cell's current backlinks as
+        Boolean values.'''
+        return {"up": self.up, "diagonal": self.diagonal, "left": self.left}
+
+    def set_score(self, score):
+        '''Updates this cell's score to the new one supplied.'''
+        self.score = score
+
+    def add_up_backlink(self):
+        '''Adds an up backlink to this cell.'''
+        self.up = True
+
+    def add_diagonal_backlink(self):
+        '''Adds a diagonal backlink to this cell.'''
+        self.diagonal = True
+
+    def add_left_backlink(self):
+        '''Adds a left backlink to this cell.'''
+        self.left = True
+
 class ScoringMatrix:
     '''A class implementing a scoring matrix.'''
     def __init__(self, sequence_length1, sequence_length2):
         '''Initializes a new scoring matrix with the supplied sequence
         lengths.'''
-        seql = (sequence_length1 + 1, sequnece_length2 + 1)
-        self.matrix = [[0 for x in range(seql[0])] for x in range(seql[1])]
+        seql = (sequence_length1 + 1, sequence_length2 + 1)
+        self.matrix = [[ScoringMatrixCell() for x in range(seql[0])]
+                       for x in range(seql[1])]
+
+    def get_score(self, row, column):
+        '''Gets the current score at the specified row and column.'''
+        return self.matrix[row][column].get_score()
+
+    def set_score(self, row, column, score):
+        '''Sets the score at the specified row and column.'''
+        self.matrix[row][column].set_score(score)
+
+    def get_backlinks(self, row, column):
+        '''Returns the current backlinks at the specified row and column in
+        the form of a dictionary of Boolean values for each direction.'''
+        self.matrix[row][column].get_backlinks()
