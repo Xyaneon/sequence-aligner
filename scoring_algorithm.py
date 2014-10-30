3a063e83517f88a38dd748c0687b503436af4a2b
Christopher Kyle Horton <christhehorton@gmail.com>
2014-10-30 18:50:48 -0400

    Add fill_matrix and match functions.

--- scoring_algorithm.py
+++ scoring_algorithm.py
@@ -36,3 +36,20 @@ def initialize_edges(sm):
         sm.set_score(i, 0, sm.get_score(i - 1, 0) + gap_score)
     for j in range(1, sm.get_cols()):
         sm.set_score(0, i, sm.get_score(0, i - 1) + gap_score)
+
+def fill_matrix(sm):
+    '''Uses dynamic programming to fill out a provided ScoringMatrix after
+    the edges have been initialized.
+
+    Based on pseudocode provided on page 54 of our textbook.'''
+    initialize_edges(sm)
+    for i in range(1, sm.get_rows()):
+        for j in range(1, sm.get_cols()):
+            score1 = 0
+            if sm.match(i, j):
+                score1 = sm.get_score(i - 1, j - 1) + match_score
+            else:
+                score1 = sm.get_score(i - 1, j - 1) + mismatch_score
+            score2 = sm.get_score(i, j - 1) + gap_score
+            score3 = sm.get_score(i - 1, j) + gap_score
+            sm.set_score(i, j, max(score1, score2, score3))
