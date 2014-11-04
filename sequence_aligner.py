35f3d2289c9524602899465f42ce21effeff792b
Christopher Kyle Horton <christhehorton@gmail.com>
2014-11-04 02:45:47 -0500

    Set up argument parsing and FASTA processing in main module.

--- sequence_aligner.py
+++ sequence_aligner.py
@@ -17,9 +17,61 @@
 
 # MCS 5603 Intro to Bioinformatics, Fall 2014
 # Christopher Kyle Horton (000516274), chorton@ltu.edu
-# Last modified: 10/28/2014
+# Last modified: 11/4/2014
 
 import argparse
 import os.path
 
 from scoring_matrix import ScoringMatrix
+
+version = "v0.0.0"
+desc = "sequence-aligner " + version
+desc += "\nFinds semi-global alignments between FASTA sequences."
+infile_help="""
+Reads in the sequence from the given file path if the file exists.
+Otherwise, treats this as a sequence string to align.
+"""
+
+#============================================================================
+# Main program code
+#============================================================================
+
+# Set up commandline argument parser
+parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=desc
+            )
+parser.add_argument("sequence1", help=infile_help)
+parser.add_argument("sequence2", help=infile_help)
+args = parser.parse_args()
+
+# Read in sequences from FASTA files, if they exist
+# Ignore first line since that's just header info, not part of the sequence
+path1, path2 = args.sequence1, args.sequence2
+sequence1 = sequence2 = ""
+if os.path.exists(path1):
+    try:
+        with open(path1, 'r') as infile1_reading:
+            lines1 = infile1_reading.readlines()[1:]
+        for line in lines1:
+            sequence1 += line.upper().strip()
+    except IOError:
+        print "Error: could not open first file:", path1
+        exit(1)
+else:
+    sequence1 = path1
+if os.path.exists(path2):
+    try:
+        with open(path2, 'r') as infile2_reading:
+            lines2 = infile2_reading.readlines()[1:]
+        for line in lines2:
+            sequence2 += line.upper().strip()
+    except IOError:
+        print "Error: could not open second file:", path2
+        exit(1)
+else:
+    sequence2 = path2
+
+sm = ScoringMatrix(sequence1, sequence2)
+
+# TODO: Finish rest of main program
