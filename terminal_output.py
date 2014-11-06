39f5104d0c68f14f028a75ea99f00c4f1a221f0a
Christopher Kyle Horton <christhehorton@gmail.com>
2014-11-06 01:21:24 -0500

    Fix alignment of top sequence in terminal output of table.

--- terminal_output.py
+++ terminal_output.py
@@ -31,7 +31,7 @@ def print_bottom_border(columns):
 def print_matrix(sm):
     '''Prints the given ScoringMatrix to the terminal.'''
     # Sequence on top
-    output_row = " |"
+    output_row = " |  |"
     top_sequence = sm.get_top_sequence()
     left_sequence = sm.get_left_sequence()
     for c in top_sequence:
